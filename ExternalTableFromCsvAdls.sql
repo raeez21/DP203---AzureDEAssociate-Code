aa935f77ef542a16fb7c5b89d8d19b682c8871e9
raeez21 <mohammadraeez.mec@gmail.com>
2024-06-20 21:45:12 +0100

    Adding some comments

--- ExternalTableFromCsvAdls.sql
+++ ExternalTableFromCsvAdls.sql
@@ -1,10 +1,13 @@
+-- Create a new database, we no longer use the default master DB in Synapse because it is for system based tables
 CREATE DATABASE [appdb]
 
+-- Create a new data source which points to our container in ADLS (Note that the path doesnt contain the file name)
 CREATE EXTERNAL DATA SOURCE srcActivityLog
 WITH(
     LOCATION = 'https://adlsraeez.blob.core.windows.net/csv/'
 )
 
+-- Creating a file format of CSV
 CREATE EXTERNAL FILE FORMAT delimitedTextFormat
 WITH(
     FORMAT_TYPE = DELIMITEDTEXT,
@@ -14,6 +17,7 @@ WITH(
     )
 );
 
+-- Create a new external table
 CREATE EXTERNAL TABLE ActivityLog
 (
     Correlationid VARCHAR(200),
@@ -29,8 +33,8 @@ CREATE EXTERNAL TABLE ActivityLog
     [Resource] VARCHAR(2000) --- because Resource is a keyword, put this in square brackets
 )
 WITH(
-    LOCATION = 'Log.csv',
-    DATA_SOURCE = srcActivityLog,
+    LOCATION = 'Log.csv', -- Name of the file in container 
+    DATA_SOURCE = srcActivityLog, 
     FILE_FORMAT = delimitedTextFormat
 );
 
