c2f7d53d00bec88807e8d3c7762cd9ac203bfade
raeez21 <mohammadraeez.mec@gmail.com>
2024-07-08 19:25:06 +0100

    Scala code first

---
+++ ScalaPythonPrograms.sql
@@ -0,0 +1,10 @@
+-- Install JDK and Scala into windows machine.
+-- Open a terminal and give below code
+
+-- sbt new scala/scala3.g8
+-- This creates a skeleton of scala project using scala version 3
+-- Name the project folder and open it in Visual Studio Code
+
+-- got to src/main/scala/Main.scala and run the below command
+-- scala Main.scala
+-- You can see the outputs in cmd
