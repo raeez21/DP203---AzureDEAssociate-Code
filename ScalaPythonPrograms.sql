1445533b73308d740919ee92615b22242c07f545
raeez21 <mohammadraeez.mec@gmail.com>
2024-07-10 16:16:01 +0100

    Spark pool

--- ScalaPythonPrograms.sql
+++ ScalaPythonPrograms.sql
@@ -8,3 +8,21 @@
 -- got to src/main/scala/Main.scala and run the below command
 -- scala Main.scala
 -- You can see the outputs in cmd
+
+
+-- Create a new Spark pool from the Synapse workspace page in Azure
+-- Create new notebook 'NotebookBasics'
+
+
+
+--In this task we take data files from ADLS and put them into a table in synapse dedciated SQL pool
+SELECT * FROM logdata_parquet
+DELETE FROm logdata_parquet
+
+--internaldb.logdatanew is a table created in Spark pool in a notebook whihc can be acessed from serverless SQL pool
+-- Change the 'Connect to' above to 'Built-in' to use the serverless SQL pool and choose 'database' as 'internaldb'
+-- Give the below command and show its working from serverless sql pool
+SELECT * FROM logdatanew
+
+
+
